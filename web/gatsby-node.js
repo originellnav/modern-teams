57bffdb877d6ddcbf5123785d73333e93da9ed76
tysbrown <tylershaynebrown@gmail.com>
2021-06-21 08:19:08 -0400

    Revert "React-carousel causing Netlify build to fail,"
    This reverts commit 4f60aea8f5905083e0ff0f60b9e0d64158499f40.

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -120,17 +120,3 @@ exports.createResolvers = ({ createResolvers }) => {
   };
   createResolvers(resolvers);
 };
-
-module.exports.onPreInit = () => {
-  const carouselSrcFile =
-    "node_modules/@brainhubeu/react-carousel/lib/react-carousel.js";
-
-  if (!fs.existsSync(carouselSrcFile)) return;
-
-  fs.writeFileSync(
-    carouselSrcFile,
-    fs
-      .readFileSync(carouselSrcFile, "utf8")
-      .replace("(window,", `(typeof window==='undefined'?{}:window,`)
-  );
-};
