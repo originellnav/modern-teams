07fb4aa4639619ecb92fbdbe119ee65b1e0ec3b3
tysbrown <tylershaynebrown@gmail.com>
2021-06-23 08:12:40 -0400

    Built out navigation schema and implemented it on the front-end.
    * Created nav, navItem, and link schemas.
    
    * Wrote new nav bar map that conditionally renders either an anchor tag
    or a link depending on whether it's an internal or external link.
    
    * Added social media icons/links to footer.

--- web/src/components/navBar.js
+++ web/src/components/navBar.js
@@ -1,43 +1,81 @@
 import React from "react";
 import * as styles from "./navBar.module.css";
 import { StaticImage } from "gatsby-plugin-image";
-import { Link } from "gatsby";
+import { Link, StaticQuery, graphql } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
-    <div className={styles.root}>
-      <div className={styles.wrapper}>
-        <div>
-          <Link to="/">
-            <StaticImage
-              src="../images/tentativeLogo.png"
-              alt="Modern Teams Logo"
-              height={40}
-              placeholder="blurred"
-            />
-          </Link>
+    <StaticQuery
+      query={graphql`
+        query HeaderNav {
+          nav: allSanityNav(filter: { title: { eq: "Main Navigation" } }) {
+            edges {
+              node {
+                navItems {
+                  text
+                  _key
+                  navItemUrl {
+                    linkUrl
+                    externalContent
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data) => (
+        <div className={styles.root}>
+          <div className={styles.wrapper}>
+            <div>
+              <Link to="/">
+                <StaticImage
+                  src="../images/tentativeLogo.png"
+                  alt="Modern Teams Logo"
+                  height={40}
+                  placeholder="blurred"
+                />
+              </Link>
+            </div>
+            <nav>
+              {data &&
+                data.nav.edges.map(({ node: nav }) => (
+                  <>
+                    <span className={styles.desktopNav}>
+                      {nav.navItems.map((navItems) => (
+                        <span key={navItems._key}>
+                          {navItems.navItemUrl.externalContent ? (
+                            <a
+                              href={navItems.navItemUrl.linkUrl}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className={styles.navLink}
+                            >
+                              {navItems.text}
+                            </a>
+                          ) : (
+                            <Link
+                              to={navItems.navItemUrl.linkUrl}
+                              className={navItems.navItemUrl.}
+                            >
+                              {navItems.text}
+                            </Link>
+                          )}
+                        </span>
+                      ))}
+                    </span>
+                  </>
+                ))}
+            </nav>
+            <div className={styles.menu}>
+              <FontAwesomeIcon icon={faBars} size="lg" />
+            </div>
+          </div>
         </div>
-        <nav className={styles.desktopNav}>
-          <Link to="/" className={styles.navLink}>
-            Companies
-          </Link>
-          <Link to="/about/" className={styles.navLink}>
-            About
-          </Link>
-          <Link to="/archive/" className={styles.navLink}>
-            Blog
-          </Link>
-          <Link to="/getlisted/" className={styles.button}>
-            Get Listed
-          </Link>
-        </nav>
-        <div className={styles.menu}>
-          <FontAwesomeIcon icon={faBars} size="lg" />
-        </div>
-      </div>
-    </div>
+      )}
+    />
   );
 };
 
