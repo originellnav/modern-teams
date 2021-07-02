26fa2df87d7b47a5b3cce07d1cf9d54961ad7a71
tysbrown <tylershaynebrown@gmail.com>
2021-07-02 10:03:17 -0400

    Notifcation success, fixing what I broke

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -17,3 +17,31 @@ module.exports = {
     {
       resolve: "gatsby-plugin-mailchimp",
       options: {
+        endpoint:
+          "https://chasethesun.us2.list-manage.com/subscribe/post?u=e0f8e5fc98940771a7df53f58&amp;id=c52d72b84c",
+        timeout: 3500,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-layout",
+      options: {
+        component: require.resolve(`./src/containers/layout.js`),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon.png",
+      },
+    },
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        ...clientConfig.sanity,
+        token: process.env.SANITY_READ_TOKEN,
+        watchMode: !isProd,
+        overlayDrafts: !isProd,
+      },
+    },
+  ],
+};
