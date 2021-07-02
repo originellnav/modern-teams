98a95360e211577040a1120a6354d0771e5a6747
tysbrown <tylershaynebrown@gmail.com>
2021-07-01 20:05:38 -0400

    Created company singleton page schema

---
+++ studio/schemas/documents/singletonCompanies.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Companies",
+  name: "companies",
+  type: "document",
+  fields: [
+    {
+      title: "Title",
+      name: "title",
+      type: "string",
+    },
+  ],
+};
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -14,6 +14,7 @@ import siteSettings from "./documents/siteSettings";
 import singletonHomepage from "./documents/singletonHomepage";
 import company from "./documents/company";
 import singletonAbout from "./documents/singletonAbout";
+import singletonCompanies from "./documents/singletonCompanies";
 
 // Object types
 import socialItem from "./objects/socialItem";
@@ -44,6 +45,7 @@ export default createSchema({
     company,
     singletonHomepage,
     singletonAbout,
+    singletonCompanies,
     siteSettings,
     post,
     category,
